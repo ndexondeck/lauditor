e4ca217786d0987b2d85ff1cca5e460013dfddea
Ohadoma Nduka <nduka.ohadoma@upperlink.ng>
2018-02-08 18:31:21 +0100

    Added missing active hour model

---
+++ sample/Model/ActiveHour.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App;
+
+class ActiveHour extends Authorization
+{
+    protected $fillable = ['name', 'begin_time', 'end_time'];
+
+    public function audits(){
+        return $this->morphMany('App\Audit', 'trail');
+    }
+
+    public function staff() {
+        return $this->hasMany('App\Staff');
+    }
+
+    public function groups() {
+        return $this->hasMany('App\Group');
+    }
+
+    public function scopeEnabled($q){
+        $q->where(function($q){
+            $q->whereEnabled('1')->orWhere('enabled','2');
+        });
+    }
+
+    public function scopeDisabled($q){
+        $q->where(function($q){
+            $q->whereEnabled('0')->orWhere('enabled','3');
+        });
+    }
+
+    public function getBeginTimeRawAttribute() {
+        return $this->attributes['begin_time'];
+    }
+
+    public function getEndTimeRawAttribute() {
+        return $this->attributes['end_time'];
+    }
+
+    public function getBeginTimeAttribute() {
+        return date('H:i',strtotime($this->attributes['begin_time']));
+    }
+    public function getEndTimeAttribute() {
+        return date('H:i',strtotime($this->attributes['end_time']));
+    }
+}
